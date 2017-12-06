f5cf94b70b6305bf79bfb018f73c46916fba8f32
hkayesh <iit12345>
2017-12-06 18:52:21 +0600

    Added command prompt input for system A

--- analyzer_main.py
+++ analyzer_main.py
@@ -1,22 +1,24 @@
 from scripts.processing import Processor
 import time
-'''
-#********** READ ME *********#
-To run the program provide three file names and the number of maximum 
-review comments to be processed. Files are 'training_file', 'data_file' and 'output_file'. 
-Please save 'training_file' and 'data_file' in the same directory to this file. The system 
-trains a classifier model using annotated data from 'training_file'. The model is then 
-used to extract aspects from a specified number of review comments ('max_reviews') 
-from 'data_file'. Finally, the result is saved as a CSV file in 'output_file'.
-'''
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--train", type=str, default='files/mmhsct_dataset.csv', help="--train file_path")
+parser.add_argument("--data", type=str, default='files/sr_all_comments_111.csv', help="--data file_path")
+parser.add_argument("--output", type=str, default='files/system_a_output.csv', help="--output file_path")
+
+args = parser.parse_args()
+training_file = args.train
+data_file = args.data
+output_file = args.output
 
 settings = {
-    'training_file': 'mmhsct_train_111.csv',
-    'data_file': 'mmhsct_test_111.csv',
+    'training_file': training_file,
+    'data_file': data_file,
     'max_reviews': None,  # Options: 0 to any integer | default: None (all)
-    'output_file': 'mmhsct_output_111.csv'
+    'output_file': output_file
 }
-
 
 if __name__ == "__main__":
     start_time = time.time()
